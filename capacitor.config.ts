567e0c75b28c35444cc2f37a7fd3b7d34332d317
Shaheen Safi <ssafi9241@gmail.com>
2026-05-14 23:32:04 +0430

    Initial commit for SafiPay Production

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'com.safipay.app',
-  webDir: 'dist'
+  appId: 'com.safipay.app', // پیشنهاد می‌شود این با نام برند شما یکی باشد
+  appName: 'SafiPay',       // نام نمایشی اپلیکیشن
+  webDir: 'dist',
+  server: {
+    // آی‌پی مک شما که در مرحله قبل پیدا کردیم
+    url: 'http://192.168.0.112:3000', 
+    cleartext: true
+  }
 };
 
 export default config;
